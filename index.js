824eb1470f71ecc58f3bbcfd601aeef50f586ff2
Rishabh Raj <nyxveil78@gmail.com>
2025-09-25 15:31:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ while(username==""||username==null){
   username=window.prompt("Your Name:");
 }
 document.getElementById("myh1").textContent=`Hello ${username}`;
+if(username=="Soumya"||username=="soumya"||username=="Soumya Singh"||username=="soumya singh"){
+  document.getElementById("s").textContent="I am in love with you";
+}
