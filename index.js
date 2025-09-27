78b2eb7fa36d8670f11661e118121a0448d7758b
Rishabh Raj <nyxveil78@gmail.com>
2025-09-27 10:10:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ let username="";
 while(username==""||username==null){
   username=window.prompt("Your Name:");
 }
+window.alert(`Hello ${username} this is beta version of GAME FORGE.If you have any suggestion give it to my gmail at last of webpage`);
 document.getElementById("myh1").textContent=`Hello ${username}`;
